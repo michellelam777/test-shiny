a57b90eb6ffebe982fffec457fa3e71850143d5e
michellelam777 <michellelam777@gmail.com>
2023-01-22 10:30:45 -0800

    updates

--- two-file-app/global.R
+++ two-file-app/global.R
@@ -1,2 +1,18 @@
 # load packages ----
 library(shiny)
+library(lterdatasampler)
+library(tidyverse)
+library(shinyWidgets)
+library(shinycssloaders)
+
+#source("file/path/to file")
+
+# data wrangling (trout) ----
+clean_trout <- and_vertebrates |> 
+  filter(species == "Cutthroat trout") |> 
+  select(sampledate, section, species, length_mm = length_1_mm, weight_g, channel_type = unittype) |> 
+  mutate(section = case_when(
+    section == "CC" ~ "clear cut forest",
+    section == "OG" ~ "old growth forest"
+  )) |> 
+  drop_na()
--- two-file-app/ui.R
+++ two-file-app/ui.R
@@ -7,7 +7,7 @@ ui <- navbarPage(
            
            "background info here"
            
-           ), # END page 1 tabPanel
+  ), # END page 1 tabPanel
   
   # page 2: data tabPanel ----
   tabPanel(title = "Explore the Data",
@@ -16,12 +16,72 @@ ui <- navbarPage(
            tabsetPanel(
              
              #trout tab ----
-             tabPanel(
-               
-             ) # END trout tab
+             tabPanel(title = "Trout",
+                      
+                      # trout sidebarLayout ----
+                      sidebarLayout(
+                        
+                        # trout sidebar panel ----
+                        sidebarPanel(
+                          
+                          # channel type pickerInput ----
+                          shinyWidgets::pickerInput(inputId = "channel_type_input",
+                                      label = "Select channel type(s):",
+                                      choices = unique(clean_trout$channel_type),
+                                      options = pickerOptions(actionsBox = TRUE),
+                                      selected = c("C", "S"),
+                                      multiple = TRUE
+                                      ), # END pickerInput
+                          
+                          # checkboxGroupButtonInput
+                          checkboxGroupButtons(inputId = "section_input",
+                                               label = "Select a sampling section(s):",
+                                               choices = unique(clean_trout$section),
+                                               selected = c("clear cut forest"),
+                                               individual = FALSE,
+                                               justified = TRUE,
+                                               checkIcon = list(yes = icon("ok", lib = "glyphicon"),
+                                                                no = icon("remove", lib = "glyphicon")))
+                          
+                        ), # END trout sidebarPanel
+                        
+                        # trout mainPanel ----
+                        mainPanel(
+                          
+                          plotOutput(outputId = "trout_scatterplot") |> withSpinner(type = 1, color = "green")
+                          
+                        ) # END trout mainPanel
+                        
+                      ) # END trout sidebarLayout
+                      
+             ), # END trout tabPanel
+             
+             # penguin tabPanel ----
+             tabPanel(title = "Penguins",
+                      
+                      # penguin sidebarLayout ----
+                      sidebarLayout(
+                        
+                        # penguin sidebar panel ----
+                        sidebarPanel(
+                          
+                          "penguin inputs here"
+                          
+                        ), # END penguin sidebarPanel
+                        
+                        # penguin Main Panel ----
+                        mainPanel(
+                          
+                          "penguin outputs here"
+                          
+                        ) # END penguin mainPanel
+                        
+                      ) # END penguin sidebarLayout
+                      
+             ) # END penguin tabPanel
              
            ) # END tabsetPanel
            
-           ), # END data tabPanel
+  ), # END data tabPanel
   
-)
+) # END navbar Page
